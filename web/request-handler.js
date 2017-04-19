509a85ddcbb34237d7ccc84683402ee4daf77905
Carlo Las Marias <carlol@gmail.com>
2017-04-19 14:35:42 -0700

    First test passed

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,32 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
+
+  if (req.method === 'GET') {
+    console.log(req.method, req.url)
+    if (req.url === '/') req.url = '/index.html';
+    helpers.serveAssets(res, archive.paths.siteAssets + req.url, function (data) {
+      res.writeHead(200, helpers.headers);
+      res.end(data);
+    });
+  } else {
+    // res.end();
+  }
+
+  if (req.method === 'POST') {
+    var body = '';
+
+    req.on('data', function (data) {
+      body += data;
+    });
+
+    req.on('end', function (data) {
+      console.log(JSON.stringify(body));
+    });
+  }
+
 };
